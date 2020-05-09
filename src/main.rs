77c47ae0c9df937146b60fa9860d18e3ef8fc622
Albert Lee <grepinsight@gmail.com>
2020-05-09 00:53:17 -0700

    Humble beginning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use chrono::NaiveDate;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+struct History {
+    username: String,
+    timestamp: i64,
+    command: String,
+    pwd: PathBuf,
+}
+
+impl History {
+    pub fn new(username: String, timestamp: i64, command: String, pwd: PathBuf) -> History {
+        History {
+            username,
+            timestamp,
+            command,
+            pwd,
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("toy.txt").expect("Fail to read the file");
+
+    let a = History::new(
+        String::from("allee"),
+        1589008111,
+        String::from("ls"),
+        PathBuf::from("/Users/allee/src/history_search"),
+    );
+    println!("{:?}", a);
 }
